f2dc28069ebf6feff1f9fa9429fc64d786bdd660
dongdongKing <ultraraptor@naver.com>
2023-01-18 21:53:19 +0900

    feat : 컨트롤러 추가

---
+++ src/main/java/io/openlibrary/connect/BookController.java
@@ -0,0 +1,9 @@
+package io.openlibrary.connect;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+}

16d94b13b8f348b9225b2f952892337def640435
dongdongKing <ultraraptor@naver.com>
2023-01-18 21:35:50 +0900

    first commit

---
+++ src/main/java/io/openlibrary/OpenlibraryApplication.java
@@ -0,0 +1,13 @@
+package io.openlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlibraryApplication.class, args);
+	}
+
+}

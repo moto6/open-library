e30c897e57f1aca8cd93857dcf2d5808da71a97c
dongdongKing <ultraraptor@naver.com>
2023-01-21 11:18:09 +0900

    feat : 대출관련 엔티티 수정

---
+++ src/main/java/io/openlibrary/domain/Lend.java
@@ -0,0 +1,35 @@
+package io.openlibrary.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Lend {
+    @Id
+    @GeneratedValue
+    @Column(name = "RENT_ID")
+    private Long rentId;
+    @NotBlank
+    @Column(name = "ACCOUNT_ID")
+    private Long accountId;
+    @NotBlank
+    @Column(name = "BOOK_STOCK_ID")
+    private Long bookStockId;
+    @NotBlank
+    @Column(name = "LEND_TIMESTAMP")
+    private Long lendTimestamp;
+    @NotBlank
+    @Column(name = "RETRIEV_TIMESTAMP")
+    private Long retrieveTimestamp;
+    @NotBlank
+    @Column(name = "RETRIEV_EXTENSION_COUNT")
+    private Long retrieveExtensionCount;
+    @NotBlank
+    @Column(name = "IS_RETRIEVED")
+    private Boolean isRetrieved;
+
+}

37e53beeb1208265f0406c23a0e7804a536952d3
dongdongKing <ultraraptor@naver.com>
2023-01-22 21:10:02 +0900

    feature : 앱 이름 추가

--- src/main/java/io/openlibrary/OpenLibraryApplication.java
+++ src/main/java/io/openlibrary/OpenLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenlibraryApplication {
+public class OpenLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpenlibraryApplication.class, args);
+		SpringApplication.run(OpenLibraryApplication.class, args);
 	}
 
 }

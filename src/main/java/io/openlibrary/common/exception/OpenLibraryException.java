0121c3d3d311beb31f10abc56657d2bc9020c966
kelly <ultraraptor@naver.com>
2023-01-26 12:14:38 +0900

    feat : 커스텀 익셉션 생성

---
+++ src/main/java/io/openlibrary/common/exception/OpenLibraryException.java
@@ -0,0 +1,7 @@
+package io.openlibrary.common.exception;
+
+public class OpenLibraryException extends RuntimeException {
+    public OpenLibraryException(ExceptionCode exception) {
+        //super(message);
+    }
+}
